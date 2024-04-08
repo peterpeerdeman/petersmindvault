f30084696314a6f9f1b45602384a54c18f034fdf
Peter Peerdeman <peterpeerdeman@gmail.com>
2024-04-08 11:06:28 +0200

    feat(license): add license

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -11,9 +11,51 @@ export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
+    const attributeElements = {
+      "xmlns:cc": "http://creativecommons.org/ns#",
+      "xmlns:dct": "http://purl.org/dc/terms/",
+    }
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
+        <p {...attributeElements}>
+          <a property="dct:title" rel="cc:attributionURL" href="https://notes.peterpeerdeman.nl">
+            Peter's Mind Vault
+          </a>{" "}
+          by{" "}
+          <a
+            rel="cc:attributionURL dct:creator"
+            property="cc:attributionName"
+            href="https://peterpeerdeman.nl"
+          >
+            Peter Peerdeman
+          </a>
+          is licensed under &nbsp;
+          <a
+            href="http://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1"
+            target="_blank"
+            rel="license noopener noreferrer"
+            style="display:inline-block;"
+          >
+            CC BY-NC-SA 4.0
+            <img
+              style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+              src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"
+            />
+            <img
+              style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+              src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"
+            />
+            <img
+              style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+              src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1"
+            />
+            <img
+              style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+              src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"
+            />
+          </a>
+        </p>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
