e95e8ed91feed3558dcd0a5f82a6e7dc1216e6ae
Peter Peerdeman <peter@peterpeerdeman.nl>
2025-08-04 12:10:41 +0200

    fix(lastmod): re-patch changes to lastmod transformer

--- quartz/plugins/transformers/lastmod.ts
+++ quartz/plugins/transformers/lastmod.ts
@@ -1,7 +1,7 @@
 import fs from "fs"
+import path from "path"
 import { Repository } from "@napi-rs/simple-git"
 import { QuartzTransformerPlugin } from "../types"
-import path from "path"
 import { styleText } from "util"
 
 export interface Options {
@@ -12,17 +12,7 @@ const defaultOptions: Options = {
   priority: ["frontmatter", "git", "filesystem"],
 }
 
-// YYYY-MM-DD
-const iso8601DateOnlyRegex = /^\d{4}-\d{2}-\d{2}$/
-
 function coerceDate(fp: string, d: any): Date {
-  // check ISO8601 date-only format
-  // we treat this one as local midnight as the normal
-  // js date ctor treats YYYY-MM-DD as UTC midnight
-  if (typeof d === "string" && iso8601DateOnlyRegex.test(d)) {
-    d = `${d}T00:00:00`
-  }
-
   const dt = new Date(d)
   const invalidDate = isNaN(dt.getTime()) || dt.getTime() === 0
   if (invalidDate && d !== undefined) {
@@ -42,32 +32,17 @@ export const CreatedModifiedDate: QuartzTransformerPlugin<Partial<Options>> = (u
   const opts = { ...defaultOptions, ...userOpts }
   return {
     name: "CreatedModifiedDate",
-    markdownPlugins(ctx) {
+    markdownPlugins() {
       return [
         () => {
           let repo: Repository | undefined = undefined
-          let repositoryWorkdir: string
-          if (opts.priority.includes("git")) {
-            try {
-              repo = Repository.discover(ctx.argv.directory)
-              repositoryWorkdir = repo.workdir() ?? ctx.argv.directory
-            } catch (e) {
-              console.log(
-                styleText(
-                  "yellow",
-                  `\nWarning: couldn't find git repository for ${ctx.argv.directory}`,
-                ),
-              )
-            }
-          }
-
           return async (_tree, file) => {
             let created: MaybeDate = undefined
             let modified: MaybeDate = undefined
             let published: MaybeDate = undefined
 
-            const fp = file.data.relativePath!
-            const fullFp = file.data.filePath!
+            const fp = file.data.filePath!
+            const fullFp = path.isAbsolute(fp) ? fp : path.posix.join(file.cwd, fp)
             for (const source of opts.priority) {
               if (source === "filesystem") {
                 const st = await fs.promises.stat(fullFp)
@@ -77,15 +52,29 @@ export const CreatedModifiedDate: QuartzTransformerPlugin<Partial<Options>> = (u
                 created ||= file.data.frontmatter.created as MaybeDate
                 modified ||= file.data.frontmatter.modified as MaybeDate
                 published ||= file.data.frontmatter.published as MaybeDate
-              } else if (source === "git" && repo) {
+              } else if (source === "git") {
+                if (!repo) {
+                  // Get a reference to the main git repo.
+                  // It's either the same as the workdir,
+                  // or 1+ level higher in case of a submodule/subtree setup
+                  //repo = Repository.discover(file.cwd)
+                  repo = Repository.discover(file.cwd + "/content")
+                }
+
+                const trimmedPath = fp.replace("content/", "")
+
                 try {
-                  const relativePath = path.relative(repositoryWorkdir, fullFp)
-                  modified ||= await repo.getFileLatestModifiedDateAsync(relativePath)
+                  //                 modified ||= await repo.getFileLatestModifiedDateAsync(file.data.filePath!)
+                  const gitLastmodified = await repo.getFileLatestModifiedDateAsync(trimmedPath)
+                  modified ||= gitLastmodified
+                  //also use latest git date as created date, as filesystem dates dont work in remote builds
+                  created ||= gitLastmodified
                 } catch {
                   console.log(
                     styleText(
                       "yellow",
-                      `\nWarning: ${file.data.filePath!} isn't yet tracked by git, dates will be inaccurate`,
+                      `\nWarning: ${file.data
+                        .filePath!} isn't yet tracked by git, last modification date is not available for this file`,
                     ),
                   )
                 }
